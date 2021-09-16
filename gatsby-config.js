1998ab972f9d3b4de55c5d3b668900cc1b2ea4de
Teri <30319159+Terifical@users.noreply.github.com>
2021-09-16 13:29:39 +0300

    with configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,33 @@
+const path = require("path");
 module.exports = {
-  siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "TestSubject1",
-  },
-  plugins: [
-    "gatsby-plugin-styled-components",
-    "gatsby-plugin-image",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-  ],
+	siteMetadata: {
+		siteUrl: "https://www.yourdomain.tld",
+		title: "TestSubject1",
+	},
+	plugins: [
+		"gatsby-plugin-styled-components",
+		"gatsby-plugin-image",
+		"gatsby-plugin-sharp",
+		"gatsby-transformer-sharp",
+		{
+			resolve: "gatsby-plugin-page-creator",
+			options: {
+				path: path.resolve(__dirname, "src/pages"),
+				ignore: ["**/sections/*", "**/options/*", "**/elements/*"],
+			},
+		},
+		{
+			resolve: "gatsby-plugin-resolve-src",
+			options: {
+				srcPath: path.resolve(__dirname, "src"),
+			},
+		},
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `images`,
+				path: path.resolve(__dirname, `src/assets/images`),
+			},
+		},
+	],
 };
